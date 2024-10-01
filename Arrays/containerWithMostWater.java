46c4238460f3926a4059cf2dc4d260cf61d62379
andrew786e <74216051+andrew786e@users.noreply.github.com>
2024-10-01 14:28:24 -0600

    Update containerWithMostWater.java

--- Arrays/containerWithMostWater.java
+++ Arrays/containerWithMostWater.java
@@ -1,21 +1,17 @@
-public class containerWithMostWater {
-    class Solution {
-        public int maxArea(int[] height) {
-            int maxAreaVal = 0 ;
-            int arrSize = height.length - 1;
-            int top ; 
-            int width ;
-            for(int i = 0 , j = arrSize ; i < j;){
-                top = (height[j] < height[i] ? height[j] : height[i]) ;
-                width = j - i ;
-                maxAreaVal = Math.max(maxAreaVal , top * width) ;
-                if(height[j] < height[i]){
-                    j-- ;
-                }else{
-                    i++ ;
-                }
+class Solution {
+    public int maxArea(int[] height) {
+        int maxWater = 0 ;
+        int waterVolume ;
+        for(int i = 0 , j = height.length - 1 ; i < j ; ){
+            if(height[i] >= height[j]){
+                waterVolume = height[j]  * (j - i) ;
+                j-- ;
+            }else{
+                waterVolume = height[i]  * (j - i) ;
+                i++ ;
             }
-            return maxAreaVal ;
-        }
+            maxWater = Math.max(maxWater , waterVolume) ;
+        }  
+        return maxWater ;
     }
 }
